2ae9c2094345ac461eb5e75d6c624aff1b3dd919
hmacr <hmac.devo@gmail.com>
2023-09-07 20:05:35 +0530

    add styles

--- tailwind.config.js
+++ tailwind.config.js
@@ -6,15 +6,22 @@ export default {
   theme: {
     colors: {
       'pale-blue': {
-        300: 'hsl(225, 100%, 94%)',
-        500: 'hsl(225, 100%, 98%)',
+        300: 'hsl(225, 100%, 98%)',
+        500: 'hsl(225, 100%, 94%)',
       },
       'bright-blue': 'hsl(245, 75%, 52%)',
       'desaturated-blue': 'hsl(224, 23%, 55%)',
       'dark-blue': 'hsl(223, 47%, 23%)',
+      white: 'hsl(0, 0%, 100%)',
     },
     fontFamily: {
       'Red-Hat-Display': ['Red Hat Display', ...defaultTheme.fontFamily.serif],
+    },
+    extend: {
+      backgroundImage: {
+        'pattern-desktop': 'url("./images/pattern-background-desktop.svg")',
+        'pattern-mobile': 'url("./images/pattern-background-mobile.svg")',
+      },
     },
   },
 };
