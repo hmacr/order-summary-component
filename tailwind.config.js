b1b7a98777544804b6dffdf22e63af127ceaaaf3
hmacr <hmac.devo@gmail.com>
2023-09-07 19:08:01 +0530

    setup tailwindcss colors and typography

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,7 +1,20 @@
+import defaultTheme from 'tailwindcss/defaultTheme';
+
 /** @type {import('tailwindcss').Config} */
 export default {
   content: ['./index.html'],
   theme: {
-    extend: {},
+    colors: {
+      'pale-blue': {
+        300: 'hsl(225, 100%, 94%)',
+        500: 'hsl(225, 100%, 98%)',
+      },
+      'bright-blue': 'hsl(245, 75%, 52%)',
+      'desaturated-blue': 'hsl(224, 23%, 55%)',
+      'dark-blue': 'hsl(223, 47%, 23%)',
+    },
+    fontFamily: {
+      'Red-Hat-Display': ['Red Hat Display', ...defaultTheme.fontFamily.serif],
+    },
   },
 };
